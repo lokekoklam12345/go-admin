1bdd866abb831522e7408812f2db19e4b3e54854
koklam <lokekl12345@gmail.com>
2026-03-15 20:29:02 +0800

    try

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -180,7 +180,8 @@ func initRouter() {
 
 	// ⚡ 在這裡加 CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://wqs-api.lokekoklam.com", "https://www.wqs-api.lokekoklam.com"},
+		// AllowOrigins:     []string{"https://wqs-api.lokekoklam.com", "https://www.wqs-api.lokekoklam.com"},
+		AllowOrigins: []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
